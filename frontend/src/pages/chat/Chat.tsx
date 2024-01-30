0aa86eeeb776e964c714e7cbbbd03e08e9f4e71c
rowannair <154312113+rowannair@users.noreply.github.com>
2024-01-30 14:19:00 +0200

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/logo_lallemand.png";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
